3b9353983841ad6976fb6609df50937cbf366c7c
Neet-30 <neetpatel2004@gmail.com>
2025-08-17 12:31:05 +0530

    trapezium.py

---
+++ trapezium.py
@@ -0,0 +1,6 @@
+a=float(input("Enter longer length of trapezium: "))
+b=float(input("Enter shorter length of trapezium: "))
+h=float(input("Enter height: "))
+
+area=0.5*(a+b)*h
+print(f"Area of trapezium: {area}")
